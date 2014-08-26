f5a44ba6c5f9f3a6eb827662d08799bb3cf6bc69
shannon code <genecyber@gmail.com>
2014-08-26 10:40:40 -0400

    repair

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {}
+
+config.port = process.env.PORT || 3000;
+config.bitcoinip = 'localhost';
+config.bitcoinport = 9332;
+config.bitcoinuser = 'bitcoinrpc';
+config.bitcoinpass = '7pL5HeF7zV0000K8bm9CC7itatAkbo6HryNbt7Z6St';
+module.exports = config;
