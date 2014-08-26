bc8471984672c8bdb066c16b7311b1574af52630
shannon code <genecyber@gmail.com>
2014-08-26 10:07:10 -0400

    cleanup

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var config = {}
 
 config.port = process.env.PORT || 3000;
-config.bitcoinip = '162.242.208.46';
+config.bitcoinip = 'localhost';
 config.bitcoinport = 9332;
 config.bitcoinuser = 'bitcoinrpc';
-config.bitcoinpass = '7pL5HeF7zVN1BS1BK8bm9CC7itatAkbo6HryNbt7Z6St'
+config.bitcoinpass = '7pL5HeF7zVN10000K8bm9CC7itatAkbo6HryNbt7Z6St'
 module.exports = config;
