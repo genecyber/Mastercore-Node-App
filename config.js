103f4a0a06aa9282ceca31ac5bf4a35a61ca1af9
shannon code <genecyber@gmail.com>
2014-08-26 10:37:49 -0400

    minor

--- config.js
+++
@@ -1,8 +0,0 @@
-var config = {}
-
-config.port = process.env.PORT || 3000;
-config.bitcoinip = 'localhost';
-config.bitcoinport = 9332;
-config.bitcoinuser = 'bitcoinrpc';
-config.bitcoinpass = '7pL5HeF7zVN10000K8bm9CC7itatAkbo6HryNbt7Z6St'
-module.exports = config;
